64c427db4f2d18d7f52d232d0a554d364dd5ab1c
Dadaram Jadhav <dadaramjadhav@gmail.com>
2025-06-24 12:11:23 +0530

    keycloak-github

--- keycloak-github/src/main/java/com/example/KeycloakAuthCodePkce.java
+++ keycloak-github/src/main/java/com/example/KeycloakAuthCodePkce.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SecurityFlow {
+public class KeycloakAuthCodePkce {
+
     public static void main(String[] args) {
-        SpringApplication.run(SecurityFlow.class, args);
+        SpringApplication.run(KeycloakAuthCodePkce.class, args);
     }
 }
